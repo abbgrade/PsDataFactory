dc6444f4fcfa8a4cc59b2c5f1464b7de9270ccc6
Steffen <skampmann@munichre.com>
2022-11-30 09:32:04 +0100

    added dependency

--- tasks/Dependency.Tasks.ps1
+++ tasks/Dependency.Tasks.ps1
@@ -1,5 +1,6 @@
 task InstallBuildDependencies -Jobs {
     Install-Module platyPs -Scope CurrentUser -ErrorAction Stop -Verbose
+    Install-Module PsDac -Scope CurrentUser -ErrorAction Stop -Verbose
 }
 
 task InstallTestDependencies -Jobs {
